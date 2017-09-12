6e30cbba09d1cb5a7c440d67a96534c34003f1f1
SergKHAI <sergserpivskiy@gmail.com>
2017-09-12 20:10:50 +0300

    Added FullName

--- ClassWork/ClassWork/User.cs
+++ ClassWork/ClassWork/User.cs
@@ -11,6 +11,8 @@ namespace ClassWork
         //комент для гита
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Surname { get; set; }
+        public string FullName => $"{Name} {Surname}";
         public int Age { get; set; }    
     }
 }
